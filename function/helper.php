f84a9a1082008c79d5fcf38dfe1e05f38515f722
Khalidwebmail <csewebmail@gmail.com>
2021-04-01 00:28:43 +0600

    configure and install blade package

--- function/helper.php
+++ function/helper.php
@@ -1,2 +1,17 @@
 <?php
 
+use Philo\Blade\Blade;
+
+/**
+ * Use to load view
+ * @param $path
+ * @param array $data
+ */
+function view( $path, array $data = [] ) {
+    $view  = __DIR__ . '/../resources/views/';
+    $cache = __DIR__ . '/../bootstrap/cache/';
+
+    $blade = new Blade( $view, $cache );
+
+    echo $blade->view()->make( $path, $data )->render();
+}
